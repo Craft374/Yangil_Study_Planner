17ceba7b091f1a8681fe84fd303ec86df54e2a34
Craft374 <66468997+Craft374@users.noreply.github.com>
2022-08-21 21:25:31 +0900

    Start

---
+++ Assets/scripts/rt.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rt : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Resources.Load("/prefeb/c");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
