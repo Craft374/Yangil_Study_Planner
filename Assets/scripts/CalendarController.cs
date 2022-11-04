f750d4c4cbb1b6825dccd613ccca8bafc7140f7a
Craft374 <leejeongwoo1103@gmail.com>
2022-11-04 15:36:06 +0900

    민결이 개입
    으헤헤

--- Assets/scripts/CalendarController.cs
+++ Assets/scripts/CalendarController.cs
@@ -12,13 +12,17 @@ public class CalendarController : MonoBehaviour
     public GameObject _calendarPanel;
     public Text _yearNumText;
     public Text _monthNumText;
-    public static string plsplspls;
+    public static string _dayNumText;
     public GameObject _item;
 
-    public static string APM1;
+    public static Text APM1;
+    public static Text APM2;
+    public static Text APM3;
+
     public static bool AM1;
     public static bool AM2;
     public static bool AM3;
+    public static bool pandan;
 
     public static string time1;
     public static string time2;
@@ -36,15 +40,26 @@ public class CalendarController : MonoBehaviour
 
     void Start()
     {
-        APM1 = GameObject.Find("APM button1").GetComponent<Text>();
+        //텍스트 레이어 지정
+        APM1 = GameObject.Find("APM Text1").GetComponent<Text>();
+        APM2 = GameObject.Find("APM Text2").GetComponent<Text>();
+        APM3 = GameObject.Find("APM Text3").GetComponent<Text>();
+
         AM1 = true;
         AM2 = true;
         AM3 = true;
-        bool pandan = true;
+
+        note1 = "";
+        note2 = "";
+        note3 = "";
+        
+        pandan = true;
+
         /*if (!Permission.HasUserAuthorizedPermission(Permission.Camera))
         {
             Permission.RequestUserPermission(Permission.Camera);
         }*/
+
         _calendarPanel.SetActive(true);
         _calendarInstance = this;
         Vector3 startPos = _item.transform.localPosition;
@@ -71,7 +86,7 @@ public class CalendarController : MonoBehaviour
     }
     void Update()
     {
- 
+        
         
     }
     void CreateCalendar()
@@ -155,42 +170,131 @@ public class CalendarController : MonoBehaviour
         //_target.text = _yearNumText.text + "-" + _monthNumText.text + "-" + int.Parse(day).ToString("D2");
         //_calendarPanel.SetActive(false);
         Debug.Log(_yearNumText.text + "-" + _monthNumText.text + "-" + day);
-        plsplspls = Convert.ToString(day);
-        Debug.Log(plsplspls);
+        _dayNumText = Convert.ToString(day);
+        
+        string[] textValue = System.IO.File.ReadAllLines(path);
+        bool justice;
+        justice = true;
+        int i;
+        i = 0;
+        while (justice == true)
+        {
+            if (i < 9)
+            {
+                sw.WriteLine(textValue[i]);
+                i += 1;
+            }
+
+            else
+            {
+                justice = false;    
+            }
+        }
     }
 
     public void APM1Click()
     {
-        if (AM1 == true)
+        while (pandan == true)
         {
-            APM1.text = "PM";
-            AM1 = false;
-        }
+            if (AM1 == true)
+                {
+                    APM1.text = "PM";
+                    AM1 = false;
+                    break;
+                }
 
-        if (AM1 == false)
+            if (AM1 == false)
+                {
+                    APM1.text = "AM";
+                    AM1 = true;
+                    break;
+                }   
+        }
+    }
+
+    public void APM2Click()
+    {
+        while (pandan == true)
         {
-            APM1.text = "AM";
-            AM1 = true;
+            if (AM2 == true)
+                {
+                    APM2.text = "PM";
+                    AM2 = false;
+                    break;
+                }
+
+            if (AM2 == false)
+                {
+                    APM2.text = "AM";
+                    AM2 = true;
+                    break;
+                }   
+        }
+    }
+
+    public void APM3Click()
+    {
+        while (pandan == true)
+        {
+            if (AM3 == true)
+                {
+                    APM3.text = "PM";
+                    AM3 = false;
+                    break;
+                }
+
+            if (AM3 == false)
+                {
+                    APM3.text = "AM";
+                    AM3 = true;
+                    break;
+                }   
         }
     }
 
     public void Note1(InputField NoteField1)
     {
         Debug.Log("메모 테스트: " + NoteField1.text);
-        Debug.Log(plsplspls);
+        note1 = NoteField1.text;
+        Debug.Log(note1);
+    }
+
+    public void Note2(InputField NoteField2)
+    {
+        Debug.Log("메모 테스트: " + NoteField2.text);
+        note2 = NoteField2.text;
+        Debug.Log(note2);
+    }
+
+    public void Note3(InputField NoteField3)
+    {
+        Debug.Log("메모 테스트: " + NoteField3.text);
+        note3 = NoteField3.text;
+        Debug.Log(note3);
+    }
+
+    public void SaveNote()
+    {
         string fullPath = "Assets/test/";
         if (File.Exists(fullPath) == false)
         {
-            var file = File.CreateText(fullPath + _yearNumText.text + "-" + _monthNumText.text + "-" + plsplspls + ".txt");
+            var file = File.CreateText(fullPath + _yearNumText.text + "-" + _monthNumText.text + "-" + _dayNumText + ".txt");
             file.Close();
         }
         //Debug.Log(DateTime.Now.ToString("yyyy"));
-        StreamWriter sw = new StreamWriter(fullPath + _yearNumText.text + "-" + _monthNumText.text + "-" + plsplspls + ".txt");
-        string memomemo = NoteField1.text;
-        //Debug.Log(Testff.text);
-        sw.WriteLine(memomemo);
+        StreamWriter sw = new StreamWriter(fullPath + _yearNumText.text + "-" + _monthNumText.text + "-" + _dayNumText + ".txt");
+        
+        sw.WriteLine(AM1);
+        sw.WriteLine(AM2);
+        sw.WriteLine(AM3);
+
+        sw.WriteLine(note1);
+        sw.WriteLine(note2);
+        sw.WriteLine(note3);
+
         sw.Flush();
         sw.Close();
+        Debug.Log("save");
     }
 
 }
